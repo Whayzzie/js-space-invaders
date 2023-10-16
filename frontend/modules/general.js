6430c71a636322bdc3bb16d8408702c8d638cbfd
Whayzzie <63013250+Whayzzie@users.noreply.github.com>
2023-10-16 16:12:58 +0000

    Ajouts variables general.js

---
+++ frontend/modules/general.js
@@ -0,0 +1,13 @@
+var accueil = document.getElementById("accueil");
+var score = document.getElementById("score");
+var partie = document.getElementById("partie");
+
+partie.style.display = "none";
+
+document.getElementById("body").addEventListener("click", function (event) {
+
+    // Vérifie si le clic est effectué avec le bouton gauche de la souris
+    if (event.button === 0) {
+        
+    }
+})

af2f66286079ef76e941b0942e6b3370eb8cb127
Whayzzie <declercqjimmypro@gmail.com>
2023-10-19 21:27:44 +0200

    création démo jeu avec tir et déplacement

--- frontend/modules/general.js
+++ frontend/modules/general.js
@@ -3,11 +3,14 @@ var score = document.getElementById("score");
 var partie = document.getElementById("partie");
 var body = document.getElementById
 
+partie.style.display = "none";
+
 document.body.addEventListener("click", function (event) {
 
     // Vérifie si le clic est effectué avec le bouton gauche de la souris
     if (event.button === 0) {
-        partie.style.display = "none";
+        accueil.style.display = "none";
+        partie.style.display = "block"
         
     }
 })
--- frontend/modules/jeu.js
+++ frontend/modules/jeu.js
@@ -1,47 +1,80 @@
-const object = document.getElementById('joueur');
-let positionX = 950;
-let positionY = -500;
-let moveX = 0;
-let moveY = 0;
-const moveSpeed = 3; // Vitesse de déplacement
+const player = document.querySelector('#joueur');
+const gameArea = document.querySelector('#game');
+const playerWidth = player.offsetWidth;
+const gameAreaWidth = gameArea.offsetWidth;
+const projectileWidth = 2;
+const projectileHeight = 20;
+const projectileSpeed = 9;
 
-function moveObject() {
-    positionX += moveX;
-    object.style.left = positionX + 'px';
+let playerX = gameAreaWidth/2;
+let enTir = false;
 
+
+function updateGameArea() {
+    // Déplacer le joueur
+    player.style.left = `${playerX}px`;
+    requestAnimationFrame(updateGameArea);
 }
 
-document.addEventListener('keydown', function(event) {
-    switch (event.key) {
-        case 'ArrowLeft':
-            
-            if (positionX + vitesse <= zoneDeJeu.clientWidth - joueur.clientWidth) {
-                positionX += vitesse;
-            }else{
-                moveX = -moveSpeed;
-            }
-            break;
-        case 'ArrowRight':
-            moveX = moveSpeed;
-           
-            break;
+function startMoving(direction) {
+    if (direction === 'left' && playerX > 0) {
+        // Déplacer le joueur vers la gauche
+        playerX -= 6; // Ajustez la valeur pour changer la vitesse
+    } else if (direction === 'right' && playerX < gameAreaWidth - playerWidth) {
+        // Déplacer le joueur vers la droite
+        playerX += 6; // Ajustez la valeur pour changer la vitesse
     }
-    
-});
-
-document.addEventListener('keyup', function(event) {
-    switch (event.key) {
-        case 'ArrowLeft':
-        case 'ArrowRight':
-            moveX = 0;
-            break;
-    }
-});
-
-// Mettre à jour la position de l'objet en continu
-function gameLoop() {
-    moveObject();
-    requestAnimationFrame(gameLoop);
 }
 
-gameLoop();
+document.addEventListener('keydown', function (event) {
+    if (event.key === ' '  && enTir === false || event.key === 'Spacebar' && enTir === false) {
+        fireProjectile(playerX + playerWidth / 2);
+        enTir = true;
+    }
+    if (event.key === 'ArrowLeft' && playerX > 0) {
+        startMoving('left');
+    } else if (event.key === 'ArrowRight' && playerX < gameAreaWidth - playerWidth) {
+        startMoving('right');
+    }
+      // Tirer un projectile lorsque la barre d'espace est pressée
+
+});
+
+document.addEventListener('keyup', function (event) {
+    if (event.key === 'ArrowLeft' || event.key === 'ArrowRight') {
+        // Arrêter le mouvement lorsque la touche est relâchée
+    }
+});
+
+function fireProjectile(positionX = playerX) {
+    const projectile = document.createElement('div');
+    projectile.className = 'projectile';
+    projectile.style.width = projectileWidth + 'px';
+    projectile.style.height = projectileHeight + 'px';
+    projectile.style.backgroundColor = 'white';
+    projectile.style.position = 'absolute';
+    projectile.style.left = positionX - projectileWidth / 2 + 'px';
+    projectile.style.bottom = '30px'; // Starting position from the bottom of the game container
+    projectile.style.zIndex = 10;
+
+
+    gameArea.appendChild(projectile);
+
+    // Déplacer le projectile vers le haut
+    function moveProjectile() {
+        const projectileBottom = parseInt(projectile.style.bottom);
+        if (projectileBottom < gameArea.offsetHeight) {
+            projectile.style.bottom = (projectileBottom + projectileSpeed) + 'px';
+            requestAnimationFrame(moveProjectile);
+        } else {
+            // Supprimer le projectile une fois qu'il sort de la zone de jeu
+            projectile.remove();
+            enTir = false;
+        }
+    }
+
+    moveProjectile();
+}
+
+// Démarrer l'animation
+updateGameArea();
